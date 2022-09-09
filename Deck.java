368c562f4fb7b9b6b84054788f0ed931765e2113
alcarter930 <112433633+alcarter930@users.noreply.github.com>
2022-09-09 11:45:01 -0400

    Java Docs + New Methods
    Added many more comments to improve clarity. Added Java Docs. Updated combine() method to shuffle the deck. Added draw() and insert() methods. insert now included 2 methods: one inserts the card randomly, and the other includes a parameter to insert at a specified position.

--- Deck.java
+++ Deck.java
@@ -1,10 +1,21 @@
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
+
+/**
+    Deck Class
+    This implements the Card class contained elsewhere in the repository.
+    To be used as a Deck of Cards for the purpose of Poker (and likely other card games)
+    within the Java Casino
+    
+    @author Aaron Carter
+    @version September 9, 2022
+
+*/
 public class Deck {
     protected ArrayList<Card> myDeck;
 
+     //constructor. systematically creates the deck
     public Deck(){
         myDeck = new ArrayList<Card>();
         for(int i = 0; i<4;i++){
@@ -14,17 +25,47 @@ public class Deck {
         }
         shuffle();
     }
-
+    
+    //accessor
     public ArrayList<Card> getDeck(){
         return myDeck;
     }
     public void shuffle(){
         Collections.shuffle(myDeck);
     }
-    public void combine(@NotNull Deck d){
+    
+    //gives the ability to combine new decks.
+    //made with the thought of blackjack having like 4 decks in mind
+    public void combine(Deck d){
         for(Card c: d.getDeck()){
             myDeck.add(c);
         }
+        shuffle();
+    }
+    
+    //insert to add a card randomly to the deck
+    //idk when this is needed but you never know...
+    public void insert(Card c){
+        myDeck.add(c);
+        shuffle();
+    }
+    //maybe I might want to insert in a certain position?
+    //I'm thinking exploding kittends type beat
+    public boolean insert(Card c, int position){
+        try{
+            myDeck.insert(c, position);
+            return true;
+        }catch (Exception e){
+            System.out.println(e);
+            return false;   
+        }
+    }    
+    
+    //draws a card and removes it from the deck. returns said card
+    public Card draw(){
+        Card drawCard = myDeck.get(0);
+        myDeck.remove(0);
+        return drawCard;
     }
 
     public String toString(){
